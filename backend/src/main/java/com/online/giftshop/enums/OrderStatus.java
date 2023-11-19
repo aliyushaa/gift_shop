8cf0ebf7ce46a7c18b75c350d82e65a5b044e0ec
Aldiyar <aldiar001122@gmail.com>
2023-11-20 03:11:46 +0600

    update

--- backend/src/main/java/com/online/giftshop/enums/OrderStatus.java
+++ backend/src/main/java/com/online/giftshop/enums/OrderStatus.java
@@ -1,10 +1,3 @@
-/**
- * @author Gagandeep Singh
- * @email singh.gagandeep3911@gmail.com
- * @create date 2021-01-13 17:58:43
- * @modify date 2021-01-13 17:58:43
- * @desc [description]
- */
 package com.online.giftshop.enums;
 
 public enum OrderStatus {
